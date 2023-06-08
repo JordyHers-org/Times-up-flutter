f16053b86bf2bf8445b2fc4a7b2f822ed7619424
jordyhers <jordy925@gmail.com>
2023-06-08 09:21:12 +0200

    test(#18) : move pumpLandingPage in Helper class

--- test/app/landing_page_test.dart
+++ test/app/landing_page_test.dart
@@ -4,14 +4,11 @@ import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:mockito/mockito.dart';
-import 'package:parental_control/app/landing_page.dart';
 import 'package:parental_control/app/pages/child_page.dart';
 import 'package:parental_control/app/pages/parent_page.dart';
-import 'package:parental_control/services/auth.dart';
-import 'package:parental_control/services/geo_locator_service.dart';
 import 'package:parental_control/sign_in/sign_in_page.dart';
-import 'package:provider/provider.dart';
 
+import '../helpers/test_helpers.dart';
 import '../helpers/test_helpers.mocks.dart';
 
 void main() {
@@ -40,45 +37,34 @@ void main() {
     });
   }
 
-  /// Always create widgets with all the ancestors that are needed
-  /// here we have to use MaterialApp
-  Future<void> pumpLandingPage(
-    WidgetTester tester, {
-    VoidCallback? onSignedIn,
-  }) async {
-    await tester.pumpWidget(
-      Provider<AuthBase>(
-        create: (_) => mockAuth,
-        child: Provider<GeoLocatorService>(
-          create: (_) => mockGeoLocatorService,
-          child: MaterialApp(
-            home: Scaffold(
-              body: LandingPage(),
-            ),
-          ),
-        ),
-      ),
-    );
-
-    await tester.pump();
-  }
-
   testWidgets('stream waiting', (WidgetTester tester) async {
     stubOnAuthStateChangesYields([]);
-    await pumpLandingPage(tester);
+    await Helper.pumpLandingPage(
+      tester,
+      mockAuthBase: mockAuth,
+      mockGeoLocatorService: mockGeoLocatorService,
+    );
     expect(find.byType(CircularProgressIndicator), findsOneWidget);
   });
 
   testWidgets('null User', (WidgetTester tester) async {
     stubOnAuthStateChangesYields([]);
-    await pumpLandingPage(tester);
+    await Helper.pumpLandingPage(
+      tester,
+      mockAuthBase: mockAuth,
+      mockGeoLocatorService: mockGeoLocatorService,
+    );
     expect(find.byType(SignInPage), findsNWidgets(0));
   });
   testWidgets(
     'non-null User',
     (WidgetTester tester) async {
       stubOnAuthStateChangesYields([]);
-      await pumpLandingPage(tester);
+      await Helper.pumpLandingPage(
+        tester,
+        mockAuthBase: mockAuth,
+        mockGeoLocatorService: mockGeoLocatorService,
+      );
       expect(find.byType(ParentPage), findsNothing);
       expect(find.byType(ChildPage), findsNothing);
     },
--- test/helpers/test_helpers.dart
+++ test/helpers/test_helpers.dart
@@ -2,12 +2,16 @@ import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:mockito/annotations.dart';
+import 'package:parental_control/app/landing_page.dart';
 import 'package:parental_control/services/app_usage_local_service.dart';
 import 'package:parental_control/services/auth.dart';
 import 'package:parental_control/services/database.dart';
 import 'package:parental_control/services/geo_locator_service.dart';
 import 'package:parental_control/services/notification_service.dart';
 import 'package:parental_control/sign_in/email_sign_in_bloc.dart';
+import 'package:provider/provider.dart';
+
+import 'test_helpers.mocks.dart';
 
 @GenerateMocks(
   [],
@@ -58,5 +62,30 @@ class Helper {
         ),
       ),
     );
+  }
+
+  /// Always create widgets with all the ancestors that are needed
+  /// here we have to use MaterialApp
+  static Future<void> pumpLandingPage(
+    WidgetTester tester, {
+    VoidCallback? onSignedIn,
+    MockAuthBase? mockAuthBase,
+    MockGeoLocatorService? mockGeoLocatorService,
+  }) async {
+    await tester.pumpWidget(
+      Provider<AuthBase>(
+        create: (_) => mockAuthBase ?? MockAuthBase(),
+        child: Provider<GeoLocatorService>(
+          create: (_) => mockGeoLocatorService ?? MockGeoLocatorService(),
+          child: MaterialApp(
+            home: Scaffold(
+              body: LandingPage(),
+            ),
+          ),
+        ),
+      ),
+    );
+
+    await tester.pump();
   }
 }
