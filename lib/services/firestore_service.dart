da4c836ffcaca04dcc36db82559caa5b3661c5c2
jordyhers <jordy925@gmail.com>
2023-06-08 20:33:25 +0200

    feat(#75) : Merge dev into current branch and updated `firestore_service.dart`

--- lib/services/firestore_service.dart
+++ lib/services/firestore_service.dart
@@ -1,6 +1,6 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
-import 'package:flutter/foundation.dart';
 import 'package:firebase_storage/firebase_storage.dart';
+import 'package:flutter/foundation.dart';
 
 class FirestoreService {
   FirestoreService._();
