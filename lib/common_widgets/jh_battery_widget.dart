6fcdf7129bfdeb98d5ee29c53cc5ab039e5eef69
jordyhers <jordy925@gmail.com>
2023-07-02 15:03:25 +0200

    chore(#145) : add color to battery widget

--- lib/common_widgets/jh_battery_widget.dart
+++ lib/common_widgets/jh_battery_widget.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:times_up_flutter/theme/theme.dart';
 
 class JHBatteryWidget extends StatelessWidget {
   const JHBatteryWidget({Key? key, this.level}) : super(key: key);
@@ -22,7 +21,7 @@ class JHBatteryWidget extends StatelessWidget {
               margin: const EdgeInsets.all(1),
               width: level!.clamp(0, 1) * 46,
               decoration: BoxDecoration(
-                color: CustomColors.greenPrimary,
+                color: _setColor(level!),
                 borderRadius: BorderRadius.circular(3),
               ),
             ),
@@ -49,5 +48,18 @@ class JHBatteryWidget extends StatelessWidget {
         ),
       ),
     );
+  }
+
+  Color _setColor(double level) {
+    if (level > 0.7) {
+      return Colors.green;
+    }
+    if (level > 0.15 && level < 0.5) {
+      return Colors.amber;
+    }
+    if (level < 0.15) {
+      return Colors.red;
+    }
+    return Colors.green;
   }
 }
